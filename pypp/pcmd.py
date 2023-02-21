94ffa646c1bf1061608300d7f92c8fae4689a103
Krutyi-4el <60041069+Krutyi-4el@users.noreply.github.com>
2023-02-21 17:07:27 +0200

    bump version

--- pypp/pcmd.py
+++ pypp/pcmd.py
@@ -10,7 +10,7 @@ if __name__ == '__main__' and __package__ is None:
     sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) ) )
 from pypp.preprocessor import Preprocessor, OutputDirective, Action
 
-version='1.30'
+version='1.31b0'
 
 __all__ = []
 
