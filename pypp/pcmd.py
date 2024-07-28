6e0aa626c2582e2c541b37ddad846ca74ef16d74
solaluset <60041069+solaluset@users.noreply.github.com>
2024-07-28 13:33:52 +0300

    Bump version to 1.33b2

--- pypp/pcmd.py
+++ pypp/pcmd.py
@@ -10,7 +10,7 @@ if __name__ == '__main__' and __package__ is None:
     sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) ) )
 from pypp.preprocessor import Preprocessor, OutputDirective, Action
 
-version = "1.33b1"
+version = "1.33b2"
 
 __all__ = []
 
